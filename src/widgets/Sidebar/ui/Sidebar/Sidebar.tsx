7b86886e9b9fe52039d37b7144ad3abcf87f7b9f
Igor Kazmin <cazmin.igor2015@yandex.ru>
2025-01-26 16:42:37 +0300

    some refactoring

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { classNames } from '../../../shared/lib/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
-import { ThemeSwitcher } from '../../../shared/ui/ThemeSwitcher';
-import { LangSwitcher } from '../../../shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from '../../../shared/ui/Button/Button';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
